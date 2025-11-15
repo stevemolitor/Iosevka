7a21a5c0e273ada260545fed32f3e8fa626d3ba1
Steve Molitor <steve.molitor@zapier.com>
2025-11-15 16:08:21 -0600

    Add custom build plans and configurations
    - Add multiple private build plan variants (JetBrains, Emacs, Envy, etc.)
    - Add custom parameter configurations (spaced-equals)
    - Modify IdentHalfSpace in relation.ptl for better spacing
    - Add build scripts for automated font generation

---
+++ build-jb.sh
@@ -0,0 +1,5 @@
+npm run build -- ttf::IosevkaJB
+npm run build -- ttf::IosevkaJBOrg
+cp dist/IosevkaJB/TTF/* ~/Library/Fonts/
+cp dist/IosevkaJBOrg/TTF/* ~/Library/Fonts/
+
---
+++ build.sh
@@ -0,0 +1,9 @@
+# npm run build -- ttf::IosevkaEnvy
+npm run build -- ttf::IosevkaEnvyOrg
+npm run build -- ttf::IosevkaEnvyOrgSlab
+# cp dist/IosevkaEnvy/TTF/* ~/Library/Fonts/
+cp dist/IosevkaEnvyOrg/TTF/* ~/Library/Fonts/
+cp dist/IosevkaEnvyOrgSlab/TTF/* ~/Library/Fonts/
+
+
+
